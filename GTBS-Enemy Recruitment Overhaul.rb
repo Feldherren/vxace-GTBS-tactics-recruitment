eac1b4fff291e065daa98d4192c9d4079fa67b2f
Feldherren <rpaliwoda@googlemail.com>
2014-04-08 16:38:12 +0100

    Now works more or less properly? Problem was with the setup of the clone actors script - you clone regular actors which are made past the clone actor ID.

--- GTBS-Enemy Recruitment Overhaul.rb
+++ GTBS-Enemy Recruitment Overhaul.rb
@@ -1,3 +1,6 @@
+# Requires Fomar's Clone Actors script
+# With this and the clone script added, invited actors are added to the party twice under certain circumstances - observed when inviting one slime and having it kill the others?
+
 module Recruitment
   # Variable to store ID of new clone in whilst it's customised.
   NEW_ACTOR_VAR = 1
@@ -15,7 +18,7 @@ class Scene_Battle_TBS < Scene_Base
       # NEW
       $game_variables[Recruitment::NEW_ACTOR_VAR]=$game_actors[id,true]
       $game_party.add_actor($game_variables[Recruitment::NEW_ACTOR_VAR])
-      customise_clone(id, battler.enemy_id)
+      customise_clone(id, battler)
       # END NEW STUFF
     elsif (battler.actor? && !battler.death_state? && battler.team != Battler_Actor &&
                                 !battler.states.include?(GTBS::CHARM_ID))
@@ -27,12 +30,13 @@ class Scene_Battle_TBS < Scene_Base
     end
   end
   
-  def customise_clone(id, enemy_id)
-    # get weapon, armour tags from original enemy
-    #$game_actors[id].change_equip_by_id(slot_id, item_id) # for changing equipment
+  def customise_clone(id, battler)
     # get enemy name
     name = battler.name()
-    $game_actors[id].name = name
+    puts name
+    $game_actors[Recruitment::NEW_ACTOR_VAR].name = name
     # get enemy level
+    # get weapon, armour tags from original enemy
+    #$game_actors[id].change_equip_by_id(slot_id, item_id) # for changing equipment
   end
 end
