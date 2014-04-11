38e1244c4102c6eb54a1f6b6a662485e8948391b
Feldherren <rpaliwoda@googlemail.com>
2014-04-11 01:06:42 +0100

    Started work on detection of equipment notetags.

--- GTBS-Enemy Recruitment Overhaul.rb
+++ GTBS-Enemy Recruitment Overhaul.rb
@@ -4,6 +4,9 @@
 module Recruitment
   # Variable to store ID of new clone in whilst it's customised.
   NEW_ACTOR_VAR = 1
+  
+  MATCH_EQUIP = /<recruit\s*equipment\s*:\s*(\d*),\s*(\d*)>/i
+  MATCH_PARAMS = /^<recruit (MHP|MMP|ATK|DEF|MAT|MDF|AGI|LUK)\s*:\s*(\+|\-)\s*([\d]*)>/i
 end
 
 class Scene_Battle_TBS < Scene_Base
@@ -40,7 +43,27 @@ class Scene_Battle_TBS < Scene_Base
       $game_actors[$game_variables[Recruitment::NEW_ACTOR_VAR]].level_up()
       currentLevel +=1
     end
+    
+    self.note.split(/[\r\n]+/).each { |line|
+      case line
+      #---
+        if (match = line.match(MATCH_EQUIP))
+          if match[1].to_i == 0
+            # equipment is a weapon
+            $game_party.gain_item($data_weapons[match[2].to_i], 1)
+          else
+            # equipment is something else
+            $game_party.gain_item($data_armors[match[2].to_i], 1)
+          end
+          #$game_actors[id].change_equip_by_id(slot_id, item_id) # for changing equipment
+          $game_actors[$game_variables[Recruitment::NEW_ACTOR_VAR]].change_equip_by_id(match[1].to_i, match[2].to_i)
+        end
+      #---
+      end
+    } # self.note.split
+    
     # get weapon, armour tags from original enemy
-    #$game_actors[id].change_equip_by_id(slot_id, item_id) # for changing equipment
+    
+    # get stat tags from original enemy
   end
 end
